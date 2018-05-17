895fd82746544475149cbc9baf48eee8233acf9b
LuWenYi <loveqq0409@gmail.com>
2018-05-17 23:37:18 +0800

    0518HomeWork

--- main.c
+++ main.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-	int MathGrade,EnglishGrade,AverageValue;
+	int MathGrade,EnglishGrade,AverageValue;/*變數宣告*/
 int main(int argc, char *argv[]) {
 
-	printf("MathGrade=");
-	scanf("%d",&MathGrade);
-	printf("EnglishGrade=");
-	scanf("%d",&EnglishGrade);
-	AverageValue=(MathGrade+EnglishGrade)/2;
-	printf("AverageValue=%d",AverageValue);
+	printf("MathGrade=");/*印出提示字元數學成績=*/ 
+	scanf("%d",&MathGrade);/*輸入數學成績*/ 
+	printf("EnglishGrade=");/*印出提示字元英文成績=*/
+	scanf("%d",&EnglishGrade);/*輸入英文成績*/ 
+	AverageValue=(MathGrade+EnglishGrade)/2;/*計算平均成績*/ 
+	printf("AverageValue=%d",AverageValue);/*印出平均成績*/ 
 	return 0;
 }
 
